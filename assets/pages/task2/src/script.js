52f08c2b7013ab89a88c73cf874e8c01edeba1cc
Andrej Mazurov <89501431007and@gmail.com>
2022-04-23 15:18:35 +0700

    finale

--- assets/pages/task2/src/script.js
+++ assets/pages/task2/src/script.js
@@ -10,11 +10,18 @@ document.querySelector('#search').addEventListener('click', event => {
     req.then(response => {
         var vacancies = []
         response.data.items.forEach(element => {
-            vacancies.push(`${element.employer.name} - ${element.name}, ЗП: ${element.salary != null ? (element.salary.from == null ? `до ${element.salary.to}` : `от ${element.salary.from}`) + ' ' + element.salary.currency : 'Не указана'}`);
+            vacancies.push(`${element.name}</br>    ${element.employer.name} - ЗП: ${element.salary != null ? (element.salary.from == null ? `до ${element.salary.to}` : `от ${element.salary.from}`) + ' ' + element.salary.currency : 'Не указана'}<br>  <div class="divider"></div>`);
         });
-        document.querySelector('#rest-result').textContent = vacancies.join('\n');
+        var html=`<p>`;
+        for(var i=0;i<vacancies.length;i++){
+            html+=vacancies[i]+'</br>';
+        }
+        html+='</p>';
+        document.querySelector('#rest-result').innerHTML = html;
+        
     })
     .catch(error => {
         alert(error.message);
     });
+    
 })
